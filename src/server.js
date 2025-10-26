cca1fec06642c14a7523ecb89ea06ad61a0b0629
nicchonsanchez <nicchonsanchez.ns@gmail.com>
2025-10-26 00:01:21 -0300

    Criei uma página HTML para exibir os usuários existentes

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ import { routes } from "./routes.js"
 import { extractQueryParams } from "./extract-query-params.js"
 
 const server = http.createServer(async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204); 
+        return res.end();
+    }
+
+    
     const {method, url} = req
 
     await json(req, res)
